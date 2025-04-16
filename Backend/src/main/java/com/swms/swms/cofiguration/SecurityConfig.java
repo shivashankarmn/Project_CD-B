51c19c3c6a7c09ea2aa316056c5219dd4bf1a266
shivashankarmn <131743146+shivashankarmn@users.noreply.github.com>
2025-04-16 13:56:19 +0530

    Update SecurityConfig.java

--- Backend/src/main/java/com/swms/swms/cofiguration/SecurityConfig.java
+++ Backend/src/main/java/com/swms/swms/cofiguration/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://13.208.182.51:3000")); // ✅ Allow frontend
+        configuration.setAllowedOrigins(List.of("http://15.152.112.2:3000")); // ✅ Allow frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // ✅ Allowed methods
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // ✅ Allow credentials (important for authentication)
