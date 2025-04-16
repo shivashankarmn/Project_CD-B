8fcdf5b5c852aba5bf5934849846eee5975d4561
shivashankarmn <131743146+shivashankarmn@users.noreply.github.com>
2025-04-16 14:20:27 +0530

    Update AuthController.java

--- Backend/src/main/java/com/swms/swms/controller/AuthController.java
+++ Backend/src/main/java/com/swms/swms/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://15.152.112.2:3000") // Allow frontend origin
+@CrossOrigin(origins = "http://15.152.37.2:3000") // Allow frontend origin
 
 public class AuthController {
 
